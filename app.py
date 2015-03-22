0ba387432d170a954b503aa12b3f845c28e80fbf
Mason Schneider <schneimd@rose-hulman.edu>
2015-03-21 22:13:50 -0400

    Sorted categories

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask,render_template
 app = Flask(__name__)
 import json
 
-categories = ["Spanish", "Christian", "Talk Radio", "Contemporary", "News", "Classical", "Sports", "Hits", "Alternative", "Oldies", "Jazz"]
+categories = ["Country", "Spanish", "Christian", "Talk Radio", "Contemporary", "News", "Classical", "Sports", "Hits", "Alternative", "Oldies", "Jazz"]
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", categories=categories)
+    return render_template("index.html", categories=sorted(categories))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, threaded=True,debug=True)
