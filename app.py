f8616168e1f79dcde2e837cc012c7d7d0f989e5e
Mason Schneider <schneimd@rose-hulman.edu>
2015-03-21 21:33:59 -0400

    Simplified categories

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ from flask import Flask,render_template
 app = Flask(__name__)
 import json
 
-categories = ['Rock', 'News']
+categories = ["Spanish", "Christian", "Talk Radio", "Contemporary", "News", "Classical", "Sports", "Hits", "Alternative", "Oldies", "Jazz"]
 
 @app.route('/')
 def hello_world():
     return render_template("index.html", categories=categories)
-
-@app.route("/categories")
-def cat():
-    return json.dumps(["Spanish", "Christian", "Talk Radio", "Contemporary", "News", "Classical", "Sports", "Hits", "Alternative", "Oldies", "Jazz"])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, threaded=True,debug=True)
