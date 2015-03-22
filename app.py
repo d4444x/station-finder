0ff679536b09f8fae4baa9849772b979ffd7f84e
dax <dax.earl@gmail.com>
2015-03-21 23:10:47 -0400

    Simplified categories

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 __author__ = 'daxx'
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 import json
+import db
 
 categories = ["Country", "Spanish", "Christian", "Talk Radio", "Contemporary", "News", "Classical", "Sports", "Hits", "Alternative", "Oldies", "Jazz"]
 
 @app.route('/')
 def hello_world():
     return render_template("index.html", categories=sorted(categories))
+
+@app.route('/get_station')
+def get_station():
+    city = request.args.get('username')
+    categories = request.args.get('categories')
+    state = request.args.get('state')
+    return json.dumps(db.get_stations_mason(city,state,categories))
+
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, threaded=True,debug=True)
---
+++ db.py
@@ -0,0 +1,79 @@
+import glob
+from sets import Set
+
+states = glob.glob("C:\\Users\\daxx\\PycharmProject\\station-finder\\radio_info\*.stations")
+categories = Set()
+for state in states:
+    f = open(state, "r")
+    for l in f:
+        if len(l.split("\t"))!=5:
+            continue
+        tag, station, city, _, category = l.split("\t")
+        category = category.replace("\n","")
+        if category:
+#             print category
+            categories.add(category)
+
+
+states = glob.glob("C:\\Users\\daxx\\PycharmProject\\station-finder\\radio_info\*.stations")
+
+ss = {}
+
+def matches(ls1,ls2):
+    #Category from user should be second
+    for i1 in ls1:
+        for i2 in ls2:
+            i1 = i1.lower()
+            i2 = i2.lower()
+            if i1.find(i2)!=-1:
+                return True
+    return False
+
+for state in states:
+    f = open(state, "r")
+    state = state[55:].replace(".stations","")
+    ss[state] = []
+    for l in f:
+        if len(l.split("\t"))!=5:
+            continue
+        tag, station, city, _, category = l.split("\t")
+        category = category.replace("\n","")
+        category = category.split("/")
+        if category!=[""]:
+            ss[state].append([tag, station, city, category])
+
+def get_stations_matchin_category(state,category):
+    stations = []
+    for tag, station, city, cat in ss[state]:
+        if matches(cat,category):
+            stations.append([tag,cat])
+    return stations
+
+def filter_by_category(category,stations):
+    stations_r = []
+    for s in stations:
+        tag, station, city, cat = s
+        if matches(cat, category):
+            stations_r.append(s)
+    return stations_r
+
+def filter_by_city(cities, stations):
+    stations_r = []
+    for s in stations:
+        tag, station, city, cat = s
+        if matches([city], cities):
+            stations_r.append(s)
+    return stations_r
+
+def get_stations_asher(categories, city_state):
+    for states,city in city_state:
+        stations = []
+        for state in states:
+            pass
+
+def get_stations_mason(city,state,categories):
+    stations = ss[state.lower().replace(" ","")]
+    stations = filter_by_city([city],stations)
+    stations = filter_by_category(categories,stations)
+    return stations
+
