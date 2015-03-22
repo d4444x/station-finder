bdb7631eb1a91cdeae9a8811bae001e2fb29164e
dax <dax.earl@gmail.com>
2015-03-21 21:18:09 -0400

    Merge remote-tracking branch 'origin/master'

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 __author__ = 'daxx'
 from flask import Flask,render_template
 app = Flask(__name__)
+import json
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route("/categories")
+def cat():
+    return json.dumps(["Spanish", "Chrisian Rock", "Punk Metal", "Heavy Hardcore", "Brony Pop"])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, threaded=True,debug=True)
